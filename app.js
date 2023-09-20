db7cf9a0365ad6b904051174d421eb4fb1c6bcc0
Burak Sürümcüoğlu <bsurumcuoglu@gmail.com>
2023-09-20 22:57:24 +0300

    conflictor on main

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("hello gh");
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./auth";
+import { uiReducer } from "./ui";
+import { categoriesReducer } from "./categories";
+import { brandsReducer } from "./brands";
+import { firmReducer } from "./firms";
+import { salesReducer } from "./sales";
+import { productsReducer } from "./products";
+import { purchasesReducer } from "./purchases";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    ui: uiReducer,
+    categories: categoriesReducer,
+    brands: brandsReducer,
+    firms: firmReducer,
+    sales: salesReducer,
+    products: productsReducer,
+    purchases: purchasesReducer,
+  },
+});
+
+export default store;
